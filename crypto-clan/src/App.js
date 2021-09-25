8ab153d32dc95bc4fc022f7283363da5103d44f3
Rishabh Das <rishabhdasufl@gmail.com>
2021-09-25 02:33:27 -0400

    run linting

--- crypto-clan/src/App.js
+++ crypto-clan/src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 
-
 function App() {
     return (
         <div className="App">
@@ -11,9 +10,6 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
                 </p>
                 <a
-
-
-
                     className="App-link"
                     href="https://reactjs.org"
                     target="_blank"
