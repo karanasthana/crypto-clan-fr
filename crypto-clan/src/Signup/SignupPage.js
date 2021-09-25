ae3ed5c84573d07ba2b55d34a516d5f4a7fe0303
Karan Asthana <karan.asthana1@gmail.com>
2021-09-25 16:09:49 -0400

    added basic HTML structuring

--- crypto-clan/src/Signup/SignupPage.js
+++ crypto-clan/src/Signup/SignupPage.js
@@ -1,3 +1,25 @@
 export default function SignupPage() {
-    return <h2>Signup</h2>;
+    return (
+        <div className="bg-purple-400" style={{ padding: '200px', paddingTop: '100px', minHeight: '100vh' }}>
+            <div className="grid grid-cols-2 divide-x divide-green-500 " style={{ backgroundColor: 'black', borderRadius: '10%'}}>
+                <div style={{ backgroundColor: 'pink' }}>
+                    <div>
+                        CRYPTO CLAN
+                    </div>
+                    <div style={{ padding: '20px' }}>
+                        <h2>Crypto Trading just became a team effort</h2>
+                    </div>
+                    <div style={{ padding: '50px' }}>
+                        <img src={'https://img.freepik.com/free-vector/online-registration-sign-up-concept-with-man-character_268404-98.jpg?size=626&ext=jpg'} />
+                    </div>
+                </div>
+                <div style={{ backgroundColor: 'yellow' }}>
+                    
+                    <h2>Login</h2>
+                    <button onClick={() => { props.history.replace('/home'); }}>
+                        Go to Dashboard
+                    </button>
+                </div>
+            </div>
+        </div>);
 }
