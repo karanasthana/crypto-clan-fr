5563799ab3c93c42fa4fc7cb5e5a36a151b4b11f
Karan Asthana <karan.asthana1@gmail.com>
2021-09-25 22:43:49 -0400

    added a new file

---
+++ crypto-clan/src/Sidebar/SidebarView.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+export default class SidebarView extends Component {
+    constructor() {
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>I am the Sidebar!</h1>
+            </div>
+        );
+    }
+}
