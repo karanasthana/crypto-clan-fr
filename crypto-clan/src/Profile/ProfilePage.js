68ad9164a50b9e89fb4ef4b54e95b1fb286f8f12
scharf-roie <scharf.roie@gmail.com>
2021-09-25 23:49:31 -0400

    HTML For Profile Page

--- crypto-clan/src/Profile/ProfilePage.js
+++ crypto-clan/src/Profile/ProfilePage.js
@@ -1,3 +1,34 @@
 export default function ProfilePage() {
-    return <h2>Profile</h2>;
+    return (
+    <div>
+        <div className="dashboard-crumb"> Dashboard {'>'} Profile </div>
+    
+        <div className="profile-header">PROFILE</div>
+    
+        <div>
+    
+            <div className="left-info">
+    
+                <div className="profile-pic"></div>
+                <div className="edit-profile-button">Edit Profile</div>
+    
+            </div>
+    
+            <div className="right-info">
+    
+                <div className="user-info">
+                    Name: inputName
+                    Email: inputEmail
+                    Joined: inputDate
+                    Clan Name: inputName
+                    Clan ID: inputID
+                    Member Total: inputTotal
+                    Assests Total: inputTotal
+                </div>
+    
+            </div>
+    
+        </div>
+    </div>
+    );
 }
