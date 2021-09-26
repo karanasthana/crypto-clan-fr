a4dc576fc418c13d56274cc62fb8c4857054fcda
Karan Asthana <karan.asthana1@gmail.com>
2021-09-26 01:49:28 -0400

    added changes to the profile page

--- crypto-clan/src/Profile/ProfilePage.js
+++ crypto-clan/src/Profile/ProfilePage.js
@@ -1,8 +1,10 @@
+import {  } from 'bootstrap';
+import { Row, Col, Image, Button } from 'react-bootstrap';
 import SidebarView from '../Sidebar/SidebarView';
 
-export default function ProfilePage() {
+export default function ProfilePage(props) {
     return (
-        <div style={{ width: '100vw' }}>
+        <div style={{ width: '100vw', minHeight: '100vh' }}>
             <div className='row'>
                 <div className='col-md-3'>
                     <SidebarView currentPage={'Profile'} />
@@ -12,24 +14,39 @@ export default function ProfilePage() {
                         Dashboard {'>'} Profile
                     </div>
     
-                    <div className="profile-header">
-                        PROFILE
+                    <div style={{ fontSize: '40px', marginBottom: '50px' }}>
+                        <strong>PROFILE</strong>
                     </div>
                     <div>
                         <div className="left-info">
-                            <div className="profile-pic"></div>
-                            <div className="edit-profile-button">Edit Profile</div>
-                        </div>
-
-                        <div className="right-info">
-                            <div className="user-info">
-                                <p> Name: inputName </p>
-                                <p>Email: inputEmail </p>
-                                <p>Joined: inputDate</p>
-                                <p>Clan Name: inputName</p>
-                                <p>Clan ID: inputID</p>
-                                <p>Member Total: inputTotal</p>
-                                <p>Assests Total: inputTotal</p>
+                            <div className="profile-pic">
+                                <Row>
+                                    <Col xs={6} md={4} >
+                                        <Image src="https://cdn.britannica.com/47/188747-050-1D34E743/Bill-Gates-2011.jpg" roundedCircle />
+                                        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '30px' }}>
+                                            <Button disabled>Edit Profile</Button>
+                                        </div>
+                                    </Col>
+                                    <Col xs={6} md={4} style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', color: '#9381FF' }}>
+                                        <div>
+                                            <p>
+                                                <strong style={{ color: 'black' }}>Name:</strong> {`${props.userName || 'John Doe'}`}
+                                            </p>
+                                            <p>
+                                                <strong style={{ color: 'black' }}>Email:</strong> {`${props.userEmail || 'john@doe.com'}`}
+                                            </p>
+                                            <p>
+                                                <strong style={{ color: 'black' }}>Clan Name:</strong> {`${props.userClanName || 'John Doe Clan'}`}
+                                            </p>
+                                            <p>
+                                                <strong style={{ color: 'black' }}>Clan ID:</strong> {`${props.userClanId || 'jdcl001'}`}
+                                            </p>
+                                            <p>
+                                                <strong style={{ color: 'black' }}>Assets Total:</strong> ${`${props.userTotal || '1200'}`}
+                                            </p>
+                                        </div>
+                                    </Col>
+                                </Row>
                             </div>
                         </div>
                     </div>
